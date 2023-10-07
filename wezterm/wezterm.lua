7131b649bf0c03fc2a77b57ef09f29fbace6a6a3
spiffyeight77 <spiffyeight77@gmail.com>
2023-10-07 23:53:09 +0900

    feat: update theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,7 +4,9 @@ return {
 	font = wezterm.font_with_fallback { 'JetBrains Mono' },
 	font_size = 14.0,
 
-	color_scheme = 'tokyonight_storm',
+	-- color_scheme = 'tokyonight_storm',
+
+	color_scheme = 'rose-pine-moon',
 
 	scrollback_lines = 3500,
 
