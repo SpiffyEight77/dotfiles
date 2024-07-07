9836759eacfcacaba561259b784b4f151c6d4f6a
Ruihua Wen <spiffyeight77@gmail.com>
2024-07-07 17:45:29 +0900

    chore: upgrade bufferline.nvim to version 4.6.1

--- nvim/lua/config/bufferline.lua
+++ nvim/lua/config/bufferline.lua
@@ -4,26 +4,36 @@ if not status then
 	return
 end
 
-
 bufferline.setup({
 	options = {
 		close_command = "Bdelete! %d",
 		right_mouse_command = "Bdelete! %d",
 
-		buffer_close_icon = '󰅖',
+		buffer_close_icon = '',
+		close_icon = '',
 		modified_icon = '●',
-		close_icon = '',
 		left_trunc_marker = '',
 		right_trunc_marker = '',
+
+		indicator = {
+			style = 'underline',
+		},
+		numbers = "ordinal",
+		max_name_length = 18,
+		max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+		truncate_names = true, -- whether or not tab names should be truncated
+		separator_style = { '', '' },
 
 		offsets = {
 			{
 				filetype = "neo-tree",
 				text = "File Explorer",
 				highlight = "Directory",
-				text_align = "left"
+				text_align = "left",
 			}
 		},
+
+		color_icons = true, -- whether or not to add the filetype icon highlights
 
 		diagnostics = "nvim_lsp",
 		---@diagnostic disable-next-line: unused-local
