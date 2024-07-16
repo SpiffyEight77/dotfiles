685c9a9f8d45c97fb4908eb6c04f7f6809fbd656
Ruihua Wen <spiffyeight77@gmail.com>
2024-07-16 23:16:43 +0900

    chore: upgrade telescope.nvim to version 0.1.8

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,7 +1,7 @@
 return {
 	-- tokyonight.nvim
 	{
-		'folke/tokyonight.nvim',
+		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
 		opts = {},
@@ -9,51 +9,51 @@ return {
 
 	-- alpha-nvim
 	{
-		'goolord/alpha-nvim',
+		"goolord/alpha-nvim",
 		config = function()
 			require("config.alpha")
-		end
+		end,
 	},
 
 	-- lualine
 	{
-		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true }
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
 	},
 
 	-- bufferline
 	{
-		'akinsho/bufferline.nvim',
+		"akinsho/bufferline.nvim",
 		version = "*",
-		dependencies = 'nvim-tree/nvim-web-devicons'
+		dependencies = "nvim-tree/nvim-web-devicons",
 	},
 
 	-- nvim-web-devicons
-	'nvim-tree/nvim-web-devicons',
+	"nvim-tree/nvim-web-devicons",
 
 	-- telescope.nvim
 	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.4',
-		dependencies = { { 'nvim-lua/plenary.nvim' } }
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
 	},
 
 	-- nvim-treesitter
 	{
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
 	},
 
 	-- indent-blankline.nvim
 	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 
 	-- gitsigns.nvim
-	'lewis6991/gitsigns.nvim',
+	"lewis6991/gitsigns.nvim",
 
 	-- mason.nvim
 	{
 		"williamboman/mason.nvim",
-		run = ":MasonUpdate" -- :MasonUpdate updates registry contents
+		run = ":MasonUpdate", -- :MasonUpdate updates registry contents
 	},
 
 	-- mason-lspconfig.nvim
@@ -63,17 +63,17 @@ return {
 	},
 
 	-- nvim-cmp
-	'hrsh7th/cmp-nvim-lsp',
-	'hrsh7th/cmp-buffer',
-	'hrsh7th/cmp-path',
-	'hrsh7th/cmp-cmdline',
-	'hrsh7th/nvim-cmp',
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/nvim-cmp",
 
 	-- vim-bbye
-	'moll/vim-bbye',
+	"moll/vim-bbye",
 
 	-- null-ls.nvim
-	'nvim-lua/plenary.nvim',
+	"nvim-lua/plenary.nvim",
 	{
 		"jose-elias-alvarez/null-ls.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
@@ -82,7 +82,9 @@ return {
 	-- nvim-autopairs
 	{
 		"windwp/nvim-autopairs",
-		config = function() require("nvim-autopairs").setup {} end
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
 	},
 
 	-- zen-mode.nvim
@@ -98,38 +100,38 @@ return {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
-		}
+		},
 	},
 
 	-- rose-pine/neovim
 	{
-		'rose-pine/neovim',
-		name = 'rose-pine',
+		"rose-pine/neovim",
+		name = "rose-pine",
 		config = function()
-			require('rose-pine').setup {
-				variant = 'moon',
-				dark_variant = 'moon',
-			}
+			require("rose-pine").setup({
+				variant = "moon",
+				dark_variant = "moon",
+			})
 		end,
 	},
 
 	-- tpope/dadbod
 	{
-		'tpope/vim-dadbod'
+		"tpope/vim-dadbod",
 	},
 
 	-- kristijanhusak/vim-dadbod-ui
 	{
-		'kristijanhusak/vim-dadbod-ui',
+		"kristijanhusak/vim-dadbod-ui",
 		dependencies = {
-			{ 'tpope/vim-dadbod',                     lazy = true },
-			{ 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
 		},
 		cmd = {
-			'DBUI',
-			'DBUIToggle',
-			'DBUIAddConnection',
-			'DBUIFindBuffer',
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
 		},
 		init = function()
 			-- Your DBUI configuration
