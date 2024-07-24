b792fb579db5e6d5966925fef370f2b319c6ac35
Ruihua Wen <spiffyeight77@gmail.com>
2024-07-24 20:26:52 +0900

    style: format lua files

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,119 +1,122 @@
-require("mason").setup({
-	ui = {
-		icons = {
-			package_installed = "✓",
-			package_pending = "➜",
-			package_uninstalled = "✗",
-		},
-	},
-})
+require("mason").setup {
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗",
+    },
+  },
+}
 
-require("mason-lspconfig").setup({
-	ensure_installed = { 'gopls', 'lua_ls', 'bufls', 'html', 'tsserver' },
-})
+require("mason-lspconfig").setup {
+  ensure_installed = { "gopls", "lua_ls", "bufls", "html", "tsserver" },
+}
 
-local lspconfig = require('lspconfig')
+local lspconfig = require "lspconfig"
 local util = require "lspconfig/util"
 
 require("mason-lspconfig").setup_handlers {
-	function(server_name)
-		require("lspconfig")[server_name].setup {}
-	end,
+  function(server_name)
+    require("lspconfig")[server_name].setup {}
+  end,
 
-	["gopls"] = function()
-		lspconfig.gopls.setup {
-			cmd = { "gopls", "serve" },
-			filetypes = { "go", "gomod" },
-			root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-			settings = {
-				gopls = {
-					analyses = {
-						unusedparams = true,
-					},
-					staticcheck = true,
-				},
-			},
-		}
-	end,
+  ["gopls"] = function()
+    lspconfig.gopls.setup {
+      cmd = { "gopls", "serve" },
+      filetypes = { "go", "gomod" },
+      root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+      settings = {
+        gopls = {
+          analyses = {
+            unusedparams = true,
+          },
+          staticcheck = true,
+        },
+      },
+    }
+  end,
 
-	["lua_ls"] = function()
-		lspconfig.lua_ls.setup {
-			settings = {
-				Lua = {
-					diagnostics = {
-						globals = { "vim" }
-					}
-				},
-			},
-		}
-	end,
+  ["lua_ls"] = function()
+    lspconfig.lua_ls.setup {
+      settings = {
+        Lua = {
+          diagnostics = {
+            globals = { "vim" },
+          },
+          format = {
+            enable = false,
+          },
+        },
+      },
+    }
+  end,
 
-	["bufls"] = function()
-		lspconfig.bufls.setup {
-			cmd = { "bufls", "serve" },
-			filetypes = { "proto" },
-			root_dir = util.root_pattern("buf.work.yaml", ".git")
-		}
-	end,
+  ["bufls"] = function()
+    lspconfig.bufls.setup {
+      cmd = { "bufls", "serve" },
+      filetypes = { "proto" },
+      root_dir = util.root_pattern("buf.work.yaml", ".git"),
+    }
+  end,
 
-	["html"] = function()
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities.textDocument.completion.completionItem.snippetSupport = true
-		lspconfig.html.setup {
-			capabilities = capabilities,
-		}
-	end,
+  ["html"] = function()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities.textDocument.completion.completionItem.snippetSupport = true
+    lspconfig.html.setup {
+      capabilities = capabilities,
+    }
+  end,
 
-	["yamlls"] = function()
-		lspconfig.yamlls.setup {
-			cmd = { "yaml-language-server", "--stdio" },
-			filetypes = { "yaml", "yaml.docker-compose" },
-			root_dir = util.find_git_ancestor,
-			settings = {
-				redhat = {
-					telemetry = {
-						enabled = false
-					}
-				}
-			}
-		}
-	end,
+  ["yamlls"] = function()
+    lspconfig.yamlls.setup {
+      cmd = { "yaml-language-server", "--stdio" },
+      filetypes = { "yaml", "yaml.docker-compose" },
+      root_dir = util.find_git_ancestor,
+      settings = {
+        redhat = {
+          telemetry = {
+            enabled = false,
+          },
+        },
+      },
+    }
+  end,
 }
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-	callback = function(ev)
-		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = { buffer = ev.buf }
-		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-		vim.keymap.set('n', '<space>k', vim.lsp.buf.hover, opts)
-		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-		vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set('n', '<space>wl', function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, opts)
-		vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-		vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-		vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-		vim.keymap.set('n', '<space>f', function()
-			vim.lsp.buf.format { async = true }
-		end, opts)
-	end,
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "<space>k", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wl", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
 })
