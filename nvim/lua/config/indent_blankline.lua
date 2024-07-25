5e1e10b5e8e2bbe8ece3c3a7a8e38b3434a441b8
Ruihua Wen <spiffyeight77@gmail.com>
2024-07-25 23:04:38 +0900

    chore: update the indent_blankline.lua

--- nvim/lua/config/indent_blankline.lua
+++ nvim/lua/config/indent_blankline.lua
@@ -1,12 +1,12 @@
 require("ibl").setup {
-	indent = {
-		char = "│",
-		tab_char = "│"
-	},
-	scope = {
-		enabled = true,
-		show_start = true,
-		show_end = true,
-		priority = 500
-	}
+  indent = {
+    char = "│",
+    tab_char = "│",
+  },
+  scope = {
+    enabled = true,
+    show_start = true,
+    show_end = true,
+    priority = 500,
+  },
 }
