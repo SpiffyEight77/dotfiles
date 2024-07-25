54975eb870ae42473ab2f7a7b31095f7c06aa5c1
Ruihua Wen <spiffyeight77@gmail.com>
2024-07-25 23:05:51 +0900

    chore: update the nvim-treesitter.lua

--- nvim/lua/config/nvim-treesitter.lua
+++ nvim/lua/config/nvim-treesitter.lua
@@ -1,9 +1,8 @@
-require 'nvim-treesitter.configs'.setup {
-	ensure_installed = { "c", "lua", "go", "html", "javascript", "query", "vimdoc" },
+require("nvim-treesitter.configs").setup {
+  ensure_installed = { "c", "lua", "go", "html", "javascript", "query", "vimdoc" },
 
-
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
 }
