6f5865537c6190d5586ce51773cf4b0b6c8a754c
spiffyeight77 <spiffyeight77@gmail.com>
2025-08-23 04:48:10 +0000

    feat: add copilot.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -68,6 +68,8 @@ return {
   "hrsh7th/cmp-path",
   "hrsh7th/cmp-cmdline",
   "hrsh7th/nvim-cmp",
+  "hrsh7th/vim-vsnip",
+  "hrsh7th/cmp-vsnip",
 
   -- vim-bbye
   "moll/vim-bbye",
@@ -161,5 +163,10 @@ return {
       "mfussenegger/nvim-dap",
       "nvim-neotest/nvim-nio",
     },
+  },
+
+  -- github/copilot.vim
+  {
+    "github/copilot.vim",
   },
 }
