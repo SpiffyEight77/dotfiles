db70bf0a29dd473ce77d59f2b5be675aa11f4605
SpiffyEight77 <spiffyeight77@gmail.com>
2026-03-21 00:20:24 +0900

    chore: clean up wezterm configuration

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,14 @@
-local wezterm = require 'wezterm';
+local wezterm = require "wezterm"
 
 return {
-	font = wezterm.font_with_fallback { 'JetBrains Mono' },
-	font_size = 14.0,
+  font = wezterm.font_with_fallback { "JetBrains Mono" },
+  font_size = 14.0,
 
-	-- color_scheme = 'tokyonight_storm',
+  color_scheme = "rose-pine-moon",
 
-	color_scheme = 'rose-pine-moon',
+  scrollback_lines = 3500,
 
-	scrollback_lines = 3500,
+  enable_tab_bar = false,
 
-	-- default_prog = { '/bin/zsh', '-c', '-l', 'tmux attach'},
-
-	enable_tab_bar = false,
-
-	window_decorations = "RESIZE",
-
-	-- window_background_opacity = 0.8,
-
-	-- macos_window_background_blur = 100,
+  window_decorations = "RESIZE",
 }
